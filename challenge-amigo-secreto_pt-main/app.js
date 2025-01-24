4fbf18399a57099a1586f30131d81178f7511de5
vlademir ribeiro <vlademirmatias1@gmail.com>
2025-01-24 15:52:26 -0300

    primeiro envio

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,12 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+//criar um array para armazenar os nomes
+
+
+
+
+
+
+
+
+
